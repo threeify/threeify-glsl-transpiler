a1054499b101b8a745ab9f854f51610eab8a7d3d
Ben Houston <bhouston@threekit.com>
2020-06-25 17:29:11 -0400

    feat: multiple include dirs, local and absolute includes, ts/js extensions

--- src/transpiler.mjs
+++ src/transpiler.mjs
@@ -2,68 +2,104 @@ import fs from "fs";
 import makeDir from "make-dir";
 import path from "path";
 
-let includeRegex = /^[ \t]*#(pragma +)?include +[<"](?<filePath>[\w\d./]+)[>"]/gm; // modified from three.js
+let includeLocalRegex = /^[ \t]*#(?:pragma +)?include +["]([\w\d./]+)["]/gm; // modified from three.js
+let includeAbsoluteRegex = /^[ \t]*#(?:pragma +)?include +[<]([\w\d./]+)[>]/gm; // modified from three.js
 let commentRegex = /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm; // https://stackoverflow.com/a/15123777
 let jsModulePrefix = "export default /* glsl */ `\n";
 let jsModulePostfix = "`;";
 
 export function glslToJavaScriptTranspiler(
-  inputDirectory,
-  inputFileName,
+  includeDirectories,
+  sourceDirectory,
+  sourceFileName,
   outputDirectory,
   outputFileName,
+  extensions,
   options
 ) {
-  let inputPath = path.dirname(inputFileName);
-  let inputSource = fs.readFileSync(inputFileName, "utf8");
+  let sourcePath = path.dirname(sourceFileName);
+  let sourceCode = fs.readFileSync(sourceFileName, "utf8");
 
-  let includeGuardName = inputFileName
-    .replace(inputDirectory, "")
+  let includeGuardName = sourceFileName
+    .replace(sourceDirectory, "")
     .replace(/[_./]/gm, "_");
 
   let includeImports = [];
 
   let errors = [];
 
-  function includeReplacer(match, includeFileName) {
-    console.log("match", match);
-    console.log("includeFileName", includeFileName);
-    if (!includeFileName) return "";
+  let searchExtensions = extensions.map((e) => "." + e);
+  searchExtensions.push("");
 
-    if (includeFileName.indexOf(".glsl") < 0) {
-      // auto add glsl extension if it is missing.
-      includeFileName += ".glsl";
-    }
-    //console.log( `includeFileName ${includeFileName}` );
-
-    var includeFilePath = path.normalize(path.join(inputPath, includeFileName));
-    //console.log( `includeFilePath ${includeFilePath}` );
-
-    if (!fs.existsSync(includeFilePath)) {
-      errors.push(
-        `#include <${includeFileName}> - Can not find the resolved target: ${includeFilePath}'`
-      );
-      return `#include <${includeFileName}> // ERROR: Can not find the resolved target: ${includeFilePath}`;
-    } else {
-      var includeVar = includeFilePath
-        .replace(inputDirectory, "")
-        .replace(/[_./]/gm, "_");
-      let includeImport = `import ${includeVar} from \'${includeFileName}.js'`;
-      if (includeImports.indexOf(includeImport) < 0) {
-        // handle multiple imports of the same file
-        includeImports.push(includeImport);
-      }
-      return "${" + includeVar + "}";
-    }
+  if (options.javascript) {
+    searchExtensions.slice(0).forEach((e) => {
+      searchExtensions.push(e + ".ts");
+      searchExtensions.push(e + ".jss");
+    });
   }
 
-  let outputSource = inputSource;
+  function includeReplacer(searchDirectories) {
+    return function (match, sourceFileName) {
+      //console.log(
+      //  "-----------------------------------------------------------------------"
+      //);
+      //console.log("resolving:", match);
+      if (!sourceFileName) return "";
 
-  if (options.removeComments) {
+      /*if (includeFileName.indexOf(".glsl") < 0) {
+      // auto add glsl extension if it is missing.
+      includeFileName += ".glsl";
+    }*/
+      //console.log( `includeFileName ${includeFileName}` );
+
+      let directories = searchDirectories.slice(0);
+      // directories.push(sourcePath);
+
+      let pathsAttempted = [];
+      var includeFilePath = undefined;
+      directories.forEach((directory) => {
+        let testIncludeFilePath = path.normalize(
+          path.join(directory, sourceFileName)
+        );
+        searchExtensions.forEach((extension) => {
+          let test2IncludeFilePath = testIncludeFilePath + extension;
+          pathsAttempted.push(test2IncludeFilePath);
+          if (fs.existsSync(test2IncludeFilePath)) {
+            includeFilePath = test2IncludeFilePath;
+          }
+        });
+      });
+
+      //console.log( `includeFilePath ${includeFilePath}` );
+
+      if (includeFilePath === undefined) {
+        const errorMsg = `Could not resolve "${match}" - current directory ${sourcePath}, attempts: ${pathsAttempted.join(
+          ","
+        )}`;
+        //console.error(errorMsg);
+        errors.push(errorMsg);
+        return errorMsg;
+      } else {
+        var includeVar = includeFilePath
+          .replace(sourceDirectory, "")
+          .replace(/[_./]/gm, "_");
+        let includeImport = `import ${includeVar} from \'${sourceFileName}.js'`;
+        if (includeImports.indexOf(includeImport) < 0) {
+          // handle multiple imports of the same file
+          includeImports.push(includeImport);
+        }
+        return "${" + includeVar + "}";
+      }
+    };
+  }
+
+  let outputSource = sourceCode;
+
+  if (!options.comments) {
     outputSource = outputSource.replace(commentRegex, "");
   }
 
-  if (inputSource.indexOf("#pragma once") >= 0) {
+  if (sourceCode.indexOf("#pragma once") >= 0) {
     let includeGuardPrefix = `#ifndef ${includeGuardName}\n#define ${includeGuardName}\n`;
     let includeGuardPostfix = `\n\n#endif // end of include guard`;
 
@@ -74,7 +110,14 @@ export function glslToJavaScriptTranspiler(
       includeGuardPostfix;
   }
 
-  outputSource = outputSource.replace(includeRegex, includeReplacer);
+  outputSource = outputSource.replace(
+    includeLocalRegex,
+    includeReplacer([sourcePath])
+  );
+  outputSource = outputSource.replace(
+    includeAbsoluteRegex,
+    includeReplacer(includeDirectories)
+  );
 
   let outputModule = includeImports.join("\n");
   if (outputModule.length > 0) {
